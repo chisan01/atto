03497090970d71398ee2cc71396ee8c3dad5db2b
alswnsp411 <alswnsp411@naver.com>
2022-05-18 13:17:44 +0900

    fix: 마이페이지 버튼(#1)

--- app/src/main/java/com/example/atto/Fragment_marcket_page_Activity.java
+++ app/src/main/java/com/example/atto/Fragment_marcket_page_Activity.java
@@ -1,10 +1,15 @@
 package com.example.atto;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,6 +17,10 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 public class Fragment_marcket_page_Activity extends Fragment {
+    private TableLayout tableLayout;
+    ImageButton mybtn;
+    Button categorybtn, brandbtn;
+
     public Fragment_marcket_page_Activity(){
 
     }
@@ -21,10 +30,20 @@ public class Fragment_marcket_page_Activity extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View fv=inflater.inflate(R.layout.activity_fragment_marcket_page, container, false);
 
-        Button categorybtn, brandbtn;
+        //마이페이지 버튼 ->마이페이지로 이동
+        mybtn=(ImageButton) fv.findViewById(R.id.mybtn);
+        mybtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //fragment에서 intent 호출할 땐 getActivity()사용
+                Intent intent=new Intent(getActivity(), MypageActivity.class);
+                startActivity(intent);
+            }
+        });
+
         categorybtn=(Button) fv.findViewById(R.id.categorybtn);
         brandbtn=(Button) fv.findViewById(R.id.brandbtn);
-
+        //카테고리 별 상품보기 fragment 호출
         categorybtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,6 +54,7 @@ public class Fragment_marcket_page_Activity extends Fragment {
             }
         });
 
+        //브랜드별 상품보기 fragment 호출
         brandbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -45,6 +65,14 @@ public class Fragment_marcket_page_Activity extends Fragment {
             }
         });
 
+        //인기상품 출력
+        tableLayout=(TableLayout) fv.findViewById(R.id.tablelayout);
+        //테이블 레이아웃에 새로운 행 추가해서 상품 출력
+        TableRow tableRow=new TableRow(getActivity().getApplicationContext());
+        tableRow.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        /* 인기상품 출력
+        for(int i=0; i<)
+*/
         return fv;
     }
 }
