8538722a944d30072ccd7a95b8fa69ee12be6b8b
안치산 <chisan2001@gmail.com>
2022-05-20 21:19:44 +0900

    Feature/#003 create product database (#5)
    * 데이터베이스 환경 설정
    
    * feat: 상품 DB 구축 완료

--- app/src/main/java/com/example/atto/Fragment_marcket_page_Activity.java
+++ app/src/main/java/com/example/atto/Fragment_marcket_page_Activity.java
@@ -1,8 +1,8 @@
 package com.example.atto;
 
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,44 +10,52 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.example.atto.database.AppDatabase;
+import com.example.atto.database.Product;
+import com.example.atto.database.ProductDao;
+import com.example.atto.database.ProductWithBrandName;
+
+import java.util.List;
+
 public class Fragment_marcket_page_Activity extends Fragment {
     private TableLayout tableLayout;
     ImageButton mybtn;
     Button categorybtn, brandbtn;
 
-    public Fragment_marcket_page_Activity(){
+    public Fragment_marcket_page_Activity() {
 
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View fv=inflater.inflate(R.layout.activity_fragment_marcket_page, container, false);
+        View fv = inflater.inflate(R.layout.activity_fragment_marcket_page, container, false);
 
         //마이페이지 버튼 ->마이페이지로 이동
-        mybtn=(ImageButton) fv.findViewById(R.id.mybtn);
+        mybtn = (ImageButton) fv.findViewById(R.id.mybtn);
         mybtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //fragment에서 intent 호출할 땐 getActivity()사용
-                Intent intent=new Intent(getActivity(), MypageActivity.class);
+                Intent intent = new Intent(getActivity(), MypageActivity.class);
                 startActivity(intent);
             }
         });
 
-        categorybtn=(Button) fv.findViewById(R.id.categorybtn);
-        brandbtn=(Button) fv.findViewById(R.id.brandbtn);
+        categorybtn = (Button) fv.findViewById(R.id.categorybtn);
+        brandbtn = (Button) fv.findViewById(R.id.brandbtn);
         //카테고리 별 상품보기 fragment 호출
         categorybtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FragmentTransaction transaction=getChildFragmentManager().beginTransaction();
+                FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
                 Fragment_marcket_category categoryFragment = new Fragment_marcket_category();
                 transaction.replace(R.id.marcketframe, categoryFragment);
                 transaction.commit();
@@ -58,7 +66,7 @@ public class Fragment_marcket_page_Activity extends Fragment {
         brandbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FragmentTransaction transaction=getChildFragmentManager().beginTransaction();
+                FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
                 Fragment_marcket_brand brandFragment = new Fragment_marcket_brand();
                 transaction.replace(R.id.marcketframe, brandFragment);
                 transaction.commit();
@@ -66,13 +74,40 @@ public class Fragment_marcket_page_Activity extends Fragment {
         });
 
         //인기상품 출력
-        tableLayout=(TableLayout) fv.findViewById(R.id.tablelayout);
-        //테이블 레이아웃에 새로운 행 추가해서 상품 출력
-        TableRow tableRow=new TableRow(getActivity().getApplicationContext());
-        tableRow.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        /* 인기상품 출력
-        for(int i=0; i<)
-*/
+        tableLayout = (TableLayout) fv.findViewById(R.id.tablelayout);
+
+        AppDatabase appDatabase = AppDatabase.getInstance(getActivity().getApplicationContext());
+        ProductDao productDao = appDatabase.productDao();
+
+        List<ProductWithBrandName> productWithBrandNameList = productDao.getAll();
+
+        for (ProductWithBrandName productWithBrandName : productWithBrandNameList) {
+            //테이블 레이아웃에 새로운 행 추가해서 상품 출력
+            TableRow tableRow = new TableRow(getActivity().getApplicationContext());
+            tableRow.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+            TextView textView = new TextView(getActivity().getApplicationContext());
+            textView.setText(productWithBrandName.name);
+            textView.setGravity(Gravity.LEFT);
+            textView.setTextSize(14);
+            tableRow.addView(textView);
+
+            TextView textView2 = new TextView(getActivity().getApplicationContext());
+            textView2.setText(productWithBrandName.category);
+            textView2.setGravity(Gravity.LEFT);
+            textView2.setTextSize(14);
+            tableRow.addView(textView2);
+
+            TextView textView3 = new TextView(getActivity().getApplicationContext());
+            if (productWithBrandName.price == -1) textView3.setText("품절");
+            else textView3.setText(productWithBrandName.price + " $");
+            textView3.setGravity(Gravity.LEFT);
+            textView3.setTextSize(14);
+            tableRow.addView(textView3);
+
+            tableLayout.addView(tableRow);
+        }
+
         return fv;
     }
 }
---
+++ app/src/main/java/com/example/atto/database/AppDatabase.java
@@ -0,0 +1,32 @@
+package com.example.atto.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Product.class, Brand.class}, version = 3)
+public abstract class AppDatabase extends RoomDatabase {
+    private static final String DB_NAME = "appDatabase.db";
+    private static volatile AppDatabase instance;
+
+    public static synchronized AppDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = create(context);
+        }
+        return instance;
+    }
+
+    private static AppDatabase create(final Context context) {
+        return Room.databaseBuilder(context, AppDatabase.class, DB_NAME)
+                .createFromAsset("database/app.db")
+                .fallbackToDestructiveMigration()
+                .allowMainThreadQueries()
+                .build();
+    }
+
+    public abstract ProductDao productDao();
+
+    public abstract BrandDao brandDao();
+}
