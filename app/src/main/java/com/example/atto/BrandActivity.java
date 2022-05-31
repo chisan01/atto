77132d537c45048228810cec802c02e6163c9ade
alswnsp411 <alswnsp411@naver.com>
2022-05-31 13:17:40 +0900

    feat: 브랜드별 상품 출력 페이지 추가

---
+++ app/src/main/java/com/example/atto/BrandActivity.java
@@ -0,0 +1,14 @@
+package com.example.atto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BrandActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_brand);
+    }
+}
